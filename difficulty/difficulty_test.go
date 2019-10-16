c377f509adbf3876ef8c5f70c48dadf725f0a5b3
Emyrk <stevenmasley@gmail.com>
2019-10-16 16:17:34 -0500

    Add corrected difficulty functions

--- difficulty/difficulty_test.go
+++ difficulty/difficulty_test.go
@@ -1,9 +1,8 @@
 package difficulty_test
 
 import (
-	"fmt"
+	"math"
 	"testing"
-	"time"
 
 	. "github.com/FactomWyomingEntity/private-pool/difficulty"
 )
@@ -14,15 +13,37 @@ const (
 	Minute uint64 = 60
 )
 
-func TestExpectedMinimumTarget(t *testing.T) {
-	fmt.Printf("%x\n", Target(100*K))
-	fmt.Printf("%x\n", ExpectedTarget(100*K, time.Minute/30))
+//func TestExpectedMinimumTarget(t *testing.T) {
+//	fmt.Printf("%x\n", TargetI(100*K))
+//	fmt.Printf("%x\n", ExpectedTarget(100*K, time.Minute/30))
+//
+//	//fmt.Println(BlocksPerTime(100*K, time.Minute*9))
+//
+//	fmt.Printf("%x\n", ExpectedMinimumTarget(100*K*MiningPeriod, 1))
+//	fmt.Printf("%x\n", ExpectedMinimumTarget(5*K*2*Minute, 1))
+//	fmt.Printf("%x\n", ExpectedMinimumTarget(5*K*Minute, 1))
+//	fmt.Printf("%x\n", ExpectedMinimumTarget(5*K*MiningPeriod, 1))
+//	fmt.Printf("%x\n", ExpectedMinimumTarget(100*K*MiningPeriod, 1))
+//}
 
-	//fmt.Println(BlocksPerTime(100*K, time.Minute*9))
+func TestDifficulty(t *testing.T) {
+	t.Run("hashrate doubling", func(t *testing.T) {
+		amt := uint64(40)
+		start := uint64(10) // First few have too much error
+		d := make([]float64, amt)
+		for i := start; i < amt; i++ {
+			target := TargetI(uint64(math.Pow(float64(2), float64(i))))
+			d[i] = Difficulty(target, PDiff)
+		}
 
-	fmt.Printf("%x\n", ExpectedMinimumTarget(100*K*MiningPeriod, 1))
-	fmt.Printf("%x\n", ExpectedMinimumTarget(5*K*2*Minute, 1))
-	fmt.Printf("%x\n", ExpectedMinimumTarget(5*K*Minute, 1))
-	fmt.Printf("%x\n", ExpectedMinimumTarget(5*K*MiningPeriod, 1))
-	fmt.Printf("%x\n", ExpectedMinimumTarget(100*K*MiningPeriod, 1))
+		for i := start + 1; i < amt; i++ {
+			if d[i]/d[i-1] != 2 {
+				t.Errorf("expect ratio of 2, found %f", d[i]/d[i-1])
+			}
+		}
+	})
+}
+
+func TestTotalHashes(t *testing.T) {
+
 }
