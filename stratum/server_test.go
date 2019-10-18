8188f39f3180408b01fccec846193173497ed4ee
Justin Hanneman <hannemennah@gmail.com>
2019-10-18 08:51:17 -0500

    Break server and client tests into separate files

--- stratum/server_test.go
+++ stratum/server_test.go
@@ -29,67 +29,6 @@ func serverAndClient(t *testing.T) (s *Server, miner *Client, srv net.Conn, cli 
 	return s, miner, srv, cli
 }
 
-func TestServer_Authorize(t *testing.T) {
-	require := require.New(t)
-	_, miner, _, cli := serverAndClient(t)
-
-	err := miner.Subscribe()
-	require.NoError(err)
-
-	r := bufio.NewReader(cli)
-	data, isPrefix, err := r.ReadLine()
-	require.NoError(err)
-	require.False(isPrefix)
-
-	err = miner.Authorize("user", "password")
-	require.NoError(err)
-
-	data, isPrefix, err = r.ReadLine()
-	require.NoError(err)
-	require.False(isPrefix)
-
-	var resp Response
-	err = json.Unmarshal(data, &resp)
-	require.NoError(err)
-	require.NotZero(resp.ID)
-	require.Nil(resp.Error)
-
-	// Check the response
-	var aRes bool
-	err = resp.FitResult(&aRes)
-	require.NoError(err)
-
-	require.True(aRes)
-}
-
-func TestServer_Subscribe(t *testing.T) {
-	require := require.New(t)
-	_, miner, _, cli := serverAndClient(t)
-
-	err := miner.Subscribe()
-	require.NoError(err)
-
-	r := bufio.NewReader(cli)
-	data, isPrefix, err := r.ReadLine()
-	require.NoError(err)
-	require.False(isPrefix)
-
-	var resp Response
-	err = json.Unmarshal(data, &resp)
-	require.NoError(err)
-	require.NotZero(resp.ID)
-	require.Nil(resp.Error)
-
-	// Check the response
-	var sRes SubscribeResult
-	err = resp.FitResult(&sRes)
-	require.NoError(err)
-
-	if len(sRes) != 2 {
-		t.Error("exp string array of 2")
-	}
-}
-
 func TestServer_Notify(t *testing.T) {
 	require := require.New(t)
 	s, _, _, cli := serverAndClient(t)
